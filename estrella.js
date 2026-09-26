d9a1ac24a8e226d4c4226fe8143d94af223dd4a5
alcantaralec <alcantaralec@gmail.com>
2026-09-25 18:43:47 -0700

    Add files via upload

---
+++ estrella.js
@@ -0,0 +1,39 @@
+function estrella () {
+
+    this.x = 0;
+    this.y = 0;
+    this.color = "#ffff00";
+
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.rotation = 0;
+
+    this.pelota = new Image();
+
+    this.pelota.onload = function () {
+        console.log("Star.png loaded!");
+    };
+
+    this.pelota.src = "Star.png";
+
+    this.radius = 75;
+}
+
+estrella.prototype.draw = function (context) {
+
+    context.save();
+
+    context.translate(this.x, this.y);
+    context.rotate(this.rotation);
+    context.scale(this.scaleX, this.scaleY);
+
+    context.drawImage(
+        this.pelota,
+        -this.radius,
+        -this.radius,
+        this.radius * 2,
+        this.radius * 2
+    );
+
+    context.restore();
+};
